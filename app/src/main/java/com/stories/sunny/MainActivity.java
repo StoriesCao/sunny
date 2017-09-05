774a9f5f2f42a46595b18bec9fbc185d6dd3cff9
Miracle <cao4618077@126.com>
2017-09-05 21:08:05 +0800

    finish UI -Now

--- app/src/main/java/com/stories/sunny/MainActivity.java
+++ app/src/main/java/com/stories/sunny/MainActivity.java
@@ -1,6 +1,8 @@
 package com.stories.sunny;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +15,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.activity_main);
 
         Window window = getWindow();
