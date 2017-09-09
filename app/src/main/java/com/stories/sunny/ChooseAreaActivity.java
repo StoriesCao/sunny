cd535395e0fca1ee1c7a6f4764ab523ac32dd76f
Miracle <cao4618077@126.com>
2017-09-09 11:00:04 +0800

    improve UI-choose area

--- app/src/main/java/com/stories/sunny/ChooseAreaActivity.java
+++ app/src/main/java/com/stories/sunny/ChooseAreaActivity.java
@@ -2,7 +2,10 @@ package com.stories.sunny;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by Charlottecao on 9/7/17.
@@ -10,10 +13,32 @@ import android.support.v7.app.AppCompatActivity;
 
 public class ChooseAreaActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_area);
+
+        toolbar = (Toolbar) findViewById(R.id.choose_area_toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_back_button);
+        }
     }
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
 }
